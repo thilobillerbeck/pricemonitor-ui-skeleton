2c5f49bad99f3d680b3eaf9b2f785f9bbadbd857
David Gengenbach <davidgengenbach@gmx.de>
2019-07-05 11:07:39 +0200

    Removed <tile/> component from example

--- example/app.ts
+++ example/app.ts
@@ -1,9 +1,9 @@
 import 'jquery';
 import * as angular from 'angular';
+import 'pricemonitor-core/dist/theme.css';
 import './services';
 import '../dist/pricemonitor.skeleton';
-
-import 'pricemonitor-core/dist/theme.css';
+import './app.less';
 
 angular
   .module('pricemonitor.skeleton.example', ['pricemonitor.skeleton']);
